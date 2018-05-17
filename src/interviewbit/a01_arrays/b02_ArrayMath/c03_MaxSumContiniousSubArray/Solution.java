0c91426a841ed4acbb8efa05b9482568e1d1ab77
Aditya Saxena <adityasmksaxena@gmail.com>
2018-05-17 19:09:27 +0530

    MaxSumContiniousSubArray

---
+++ src/interviewbit/a01_arrays/b02_ArrayMath/c03_MaxSumContiniousSubArray/Solution.java
@@ -0,0 +1,20 @@
+package interviewbit.a01_arrays.b02_ArrayMath.c03_MaxSumContiniousSubArray;
+
+import java.util.List;
+
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int maxSubArray(final List<Integer> A) {
+        int c_sum = 0;
+        int max_sum = Integer.MIN_VALUE;
+
+        for(int i=0; i<A.size(); i++) {
+            int val = A.get(i);
+            c_sum += val;
+            if(max_sum < c_sum)
+                max_sum = c_sum;
+            if(c_sum < 0) c_sum = 0;
+        }
+        return max_sum;
+    }
+}

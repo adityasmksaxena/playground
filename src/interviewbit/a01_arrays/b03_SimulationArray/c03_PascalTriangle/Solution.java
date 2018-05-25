99e08278eb84ad3ea52ccd8d1fa54328861af6d9
Aditya Saxena <adityasmksaxena@gmail.com>
2018-05-25 19:43:09 +0530

    added PascalTriangle

---
+++ src/interviewbit/a01_arrays/b03_SimulationArray/c03_PascalTriangle/Solution.java
@@ -0,0 +1,21 @@
+package interviewbit.a01_arrays.b03_SimulationArray.c03_PascalTriangle;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> generate(int A) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>(A);
+        if(A==0) return res;
+        res.add(new ArrayList<Integer>(1));
+        res.get(0).add(1);
+        for(int i=1; i<A; i++) {
+            res.add(new ArrayList<Integer>(i+1));
+            res.get(i).add(1);
+            for(int j=1; j<i; j++) {
+                res.get(i).add(res.get(i-1).get(j) + res.get(i-1).get(j-1));
+            }
+            res.get(i).add(1);
+        }
+        return res;
+    }
+}
